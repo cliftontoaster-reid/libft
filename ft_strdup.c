757502e1fd45ade1340997350fe046d823584a93
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-02-10 22:46:41 +0100

    Update GitHub Actions workflow for Francinette testing and fix ft_strdup error handling

--- ft_strdup.c
+++ ft_strdup.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strdup.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: cliftontr <cliftontr@student.42.fr>        +#+  +:+       +#+        */
+/*   By: lfiorell <lfiorell@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:11:54 by lfiorell          #+#    #+#             */
-/*   Updated: 2024/11/11 17:48:59 by cliftontr        ###   ########.fr       */
+/*   Updated: 2025/02/10 22:40:49 by lfiorell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ char	*ft_strdup(const char *s)
 
 	l = ft_strlen(s);
 	res = ft_calloc(sizeof(char), l + 1);
-	if (res <= 0)
+	if (res == NULL)
 		return (NULL);
 	ft_memcpy(res, (void *)s, l);
 	return (res);
