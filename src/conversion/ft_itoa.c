f6ecf282704a28ba29f928e2b0bd273913c71994
Clifton Toaster Reid <clifton.toaster.reid@proton.me>
2025-04-02 11:23:08 +0200

    ITOA FIX

--- src/conversion/ft_itoa.c
+++ src/conversion/ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:33:39 by lfiorell          #+#    #+#             */
-/*   Updated: 2024/11/07 18:18:17 by lfiorell         ###   ########.fr       */
+/*   Updated: 2025/04/02 11:22:54 by lfiorell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,12 +27,11 @@ static void	handle_nbr(int n, char *res, int len, int sign)
 		res[0] = '-';
 }
 
-char	*ft_itoa(int n)
+static int	get_len(int n)
 {
 	long long	nb;
 	int			sign;
 	int			len;
-	char		*res;
 
 	nb = n;
 	sign = 0;
@@ -45,6 +44,23 @@ char	*ft_itoa(int n)
 		nb /= 10 + 0 * len++;
 	if (sign == -1)
 		len++;
+	return (len);
+}
+
+char	*ft_itoa(int n)
+{
+	int		sign;
+	int		len;
+	char	*res;
+
+	if (n == INT_MIN)
+		return (ft_strdup("-2147483648"));
+	if (n == INT_MAX)
+		return (ft_strdup("2147483647"));
+	sign = 1;
+	if (n < 0)
+		sign = -1;
+	len = get_len(n);
 	res = (char *)malloc(sizeof(char) * (unsigned long)(len + 1));
 	if (res == NULL)
 		return (NULL);
--- src/fd/ft_putnbr_fd.c
+++ src/fd/ft_putnbr_fd.c
@@ -6,7 +6,7 @@
 /*   By: lfiorell <lfiorell@student.42nice.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/07 10:45:40 by lfiorell          #+#    #+#             */
-/*   Updated: 2024/11/07 18:24:41 by lfiorell         ###   ########.fr       */
+/*   Updated: 2025/04/02 11:22:59 by lfiorell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,12 @@
 
 void	ft_putnbr_fd(int n, int fd)
 {
-	int	nb;
+	char	*str;
 
-	nb = n;
-	if (nb < 0)
+	str = ft_itoa(n);
+	if (str)
 	{
-		ft_putchar_fd('-', fd);
-		nb *= -1;
+		ft_putstr_fd(str, fd);
+		free(str);
 	}
-	if (nb > 9)
-		ft_putnbr_fd(nb / 10, fd);
-	ft_putchar_fd((char)nb % 10 + '0', fd);
 }
